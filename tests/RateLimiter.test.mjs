bf883582c7dd57f2dd500fa000fc45f4024634f5
HamphreyChinyerere <hamphreytanatswa@gmail.com>
2026-08-25 14:16:25 +0200

    test: add sliding window expiration test

--- tests/RateLimiter.test.mjs
+++ tests/RateLimiter.test.mjs
@@ -17,4 +17,36 @@ describe("RateLimiter", () => {
         expect(limiter.allow("user-1")).toBe(false);
 
     })
+    test("limits each client seperately", () => {
+        const limiter = new RateLimiter(3, 10000);
+
+        expect(limiter.allow("user-1")).toBe(true);
+        expect(limiter.allow("user-1")).toBe(true);
+        expect(limiter.allow("user-1")).toBe(true);
+        expect(limiter.allow("user-1")).toBe(false);
+
+        expect(limiter.allow("user-2")).toBe(true);
+        expect(limiter.allow("user-2")).toBe(true);
+        expect(limiter.allow("user-2")).toBe(true);
+        expect(limiter.allow("user-2")).toBe(false);
+
+    })
+    test("allows a request after the window expires", () => {
+        let currentTime = 0;
+        
+        const limiter = new RateLimiter(
+            2,
+            10000,
+            () => currentTime
+        );
+        expect(limiter.allow("user-1")).toBe(true);
+        expect(limiter.allow("user-1")).toBe(true);
+        expect(limiter.allow("user-1")).toBe(false);
+
+        currentTime = 10001;
+
+        expect(limiter.allow("user-1")).toBe(true)
+    })
+    
+    
 })
